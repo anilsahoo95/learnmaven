d1da7245fb1db5c707c491aac432bc3a0344dcfc
unknown <anilsahoo95@gmail.com>
2020-10-14 11:28:31 +0530

    new commit

--- HelloApp/src/main/java/com/example/demo/HelloAppApplication.java
+++ HelloApp/src/main/java/com/example/demo/HelloAppApplication.java
@@ -2,9 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloAppApplication {
+public class HelloAppApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloAppApplication.class, args);
